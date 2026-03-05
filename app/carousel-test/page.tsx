d080afc4e0c5c40e396a417f62ab34fea496b39e
Aadhil Kassim <aadhikassim@gmail.com>
2026-03-05 19:29:11 +0530

    Repositioned Carousel component

--- app/carousel-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import Carousel from "../../components/Carousel";
-
-export default function CarouselTestPage() {
-  return (
-    <main className="min-h-screen bg-black">
-      <Carousel />
-    </main>
-  );
-}
